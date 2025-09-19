9def6f317abb517357f425e6fb69c4f625a3834c
Yashraj Shukla <125136048+Yash72170@users.noreply.github.com>
2025-09-19 17:31:50 +0530

    Update About.js

--- Frontend/src/pages/About.js
+++ Frontend/src/pages/About.js
@@ -1,22 +1,3 @@
-// import userContext from "../context/userContext";
-// import Base from "../components/Base";
-
-// const About = () => {
-//   return (
-//     <userContext.Consumer>
-//       {(object) => (
-//         <Base>
-//           <h1>This is about page</h1>
-//           <p>we are building blog website</p>
-//           {console.log(object)}
-//           <h1>Welcome user: {object.user.login && object.user.data.name}</h1>
-//         </Base>
-//       )}
-//     </userContext.Consumer>
-//   );
-// };
-
-// export default About;
 
 
 import React from "react";
