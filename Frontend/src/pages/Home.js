dc3c8421beff881d5fe9d4a2afeccb093f848305
Yashraj Shukla <125136048+Yash72170@users.noreply.github.com>
2025-09-19 17:31:28 +0530

    Update Home.js

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -1,27 +1,4 @@
-// import { useEffect } from "react";
-// import { Container, Row, Col } from "reactstrap";
-// import Base from "../components/Base";
-// import CategorySideMenu from "../components/CategorySideMenu";
-// import NewFeed from "../components/NewFeed";
 
-// const Home = () => {
-//   return (
-//     <Base>
-//       <Container className="mt-3">
-//         <Row>
-//           <Col md={2} className="pt-5">
-//             <CategorySideMenu />
-//           </Col>
-//           <Col md={10}>
-//             <NewFeed />
-//           </Col>
-//         </Row>
-//       </Container>
-//     </Base>
-//   );
-// };
-
-// export default Home;
 
 import React from "react";
 import Base from "../components/Base";
